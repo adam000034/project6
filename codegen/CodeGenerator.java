b0a9187e9845afadf5ae9d7c30ee3b64965d4c64
agauyeung <agauyeung@dones.usfca.edu>
2015-05-08 13:09:42 -0700

    correct with li

--- codegen/CodeGenerator.java
+++ codegen/CodeGenerator.java
@@ -117,10 +117,10 @@ class CodeGenerator implements AATVisitor {
                 Label andendlabel = new Label("andendlabel");
                 emit("beq " + Register.Tmp1() + ", " + 0 + ", " + andfalselabel);	//	beq $t1, 0, falselabel
                 emit("beq " + Register.ACC() + ", " + 0 + ", " + andfalselabel);	//	beq $ACC, 0, falselabel
-                emit("addi " + Register.ACC() + ", " + 1 + ", " + 0);		//	addi $ACC, 1, 0
+                emit("li " + Register.ACC() + ", " + 1);		//	addi $ACC, 1, 0
                 emit("j " + andendlabel);						//	j endlabel
                 emit(andfalselabel + ":");						//falselabel:
-                emit("addi " + Register.ACC() + ", " + 0 + ", " + 0);		//	addi $ACC, 0, 0
+                emit("li " + Register.ACC() + ", " + 0);		//	addi $ACC, 0, 0
                 emit(andendlabel + ": ");						//endlabel:
                 break;
             case AATOperator.OR:
@@ -132,10 +132,10 @@ class CodeGenerator implements AATVisitor {
                 Label orendlabel = new Label("orendlabel");
                 emit("beq " + Register.Tmp1() + ", " + 1 + ", " + ortruelabel);	//	beq $t1, 1, truelabel
                 emit("beq " + Register.ACC() + ", " + 1 + ", " + ortruelabel);	//	beq $ACC, 1, truelabel
-                emit("addi " + Register.ACC() + ", " + 0 + ", " + 0);		//	addi $ACC, 0, 0
+                emit("li " + Register.ACC() + ", " + 0);		//	addi $ACC, 0, 0
                 emit("j " + orendlabel);						//	j endlabel
                 emit(ortruelabel + ":");						//truelabel:
-                emit("addi " + Register.ACC() + ", " + 1 + ", " + 0);		//	addi $ACC, 1, 0
+                emit("li " + Register.ACC() + ", " + 1);		//	addi $ACC, 1, 0
                 emit(orendlabel + ": ");						//endlabel:
                 break;
             case AATOperator.EQUAL:
@@ -176,10 +176,10 @@ class CodeGenerator implements AATVisitor {
                 Label ntruelabel = new Label("ntruelabel");
                 Label nendlabel = new Label("nendlabel");
                 emit("beq " + Register.ACC() + ", " + 1 + ", " + ntruelabel);
-                emit("addi" + Register.ACC() + ", " + 0 + ", " + 1);
+                emit("li" + Register.ACC() + ", " + 1);
                 emit("j " + nendlabel);
                 emit(ntruelabel + ":");
-                emit("addi " + Register.ACC() + ", " + 0 + ", " + 0);
+                emit("li " + Register.ACC() + ", " + 0);
                 emit(nendlabel + ":");
                 break;
         }
@@ -290,7 +290,8 @@ class CodeGenerator implements AATVisitor {
     }   /* DONE */
     
     public Object VisitConstant(AATConstant expression) {
-        emit ("sw " + expression.value() + ", 0(" + Register.ACC() + ")");
+        emit ("li " + Register.ACC() + ", " + expression.value());
+        //addi $ACC, 0, value
         return null;
     }   /* DONE */
 //------------------------------------------------------------------------------------------
