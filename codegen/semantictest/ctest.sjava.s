59b348f8bf89a5d0ddbb4c3d040b9c58c04c67e8
agauyeung <agauyeung@dones.usfca.edu>
2015-05-09 22:16:38 -0700

    Passing every test!

---
+++ codegen/semantictest/ctest.sjava.s
@@ -0,0 +1,67 @@
+	.globl main
+main:
+	addi $t1,$sp,0
+	addi $sp,$sp,-4000
+	addi $t2,$sp,0
+	addi $t2,$t2,-4000
+	la $t3, HEAPPTR
+	sw $t2,0($t3)
+	sw $ra,4($sp)
+	jal main1
+	li $v0, 10
+	syscall
+main1:
+	sw $fp, -4($sp)
+	sw $sp, -8($sp)
+	sw $ra, -12($sp)
+	addi $fp, $sp, 0
+	addi $sp, $sp, -16
+	addi $sp, $sp, 0
+	jal Read
+	addi $sp, $sp, 0
+	addi $t0, $v0, 0
+	sw $t0, 0($fp)
+	lw $t0, 0($fp)
+	addi $t0, $t0, 1
+	sw $t0, 0($fp)
+	addi $sp, $sp, -4
+	lw $t0, 0($fp)
+	sw $t0, 4($sp)
+	jal Print
+	addi $sp, $sp, 4
+main2:
+	lw $ra, -12($fp)
+	lw $sp, -8($fp)
+	lw $fp, -4($fp)
+	jr $ra
+Print:
+	lw $a0, 4($sp)
+	li $v0, 1
+	syscall
+	li $v0,4
+	la $a0, sp
+	syscall
+	jr $ra
+Println:
+	li $v0,4
+	la $a0, cr
+	syscall
+	jr $ra
+Read:
+	li $v0,5
+	syscall
+	jr $ra
+allocate:
+	la $t2, HEAPPTR
+	lw $v0,0($t2)
+	lw $t3, 4($sp)
+	sub $t3,$v0,$t3
+	sw $t3,0($t2)
+	jr $ra
+	.data
+cr:
+	.asciiz "\n"
+sp:
+	.asciiz " "
+HEAPPTR:
+	.word 0
